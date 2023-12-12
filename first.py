b076dfdc92568234f80d3eb0f7a2fe83944c90fc
Vahan Dağ <vahandag@gmail.com>
2023-12-12 09:54:47 +0300

    part 13,14,15,16 created

--- first.py
+++ first.py
@@ -7,7 +7,7 @@ from openai import OpenAI
 client = OpenAI()
 
 # İlk JSON dosyası
-current_file_number = 9
+current_file_number = 16
 questions_file = f"questions{current_file_number}.json"
 
 # JSON dosyasını yüklemek ve dosya numarasını güncellemek için fonksiyon
@@ -38,7 +38,7 @@ while True:
             response_format={"type": "json_object"},
             messages=[
                 {"role": "system", "content": "You are a super assistant designed to output JSON and prepare questions for all Q&A applications in the world."},
-                {"role": "user", "content": """Generate 4 sets of 8 multiple-choice questions each. For each set, randomly select 8 topics from the following list and create one question per topic: History, Technology, General Knowladge, Science, Mathematics, Literature, Cinema & TV, Music, Geography, Sports, Computer Science, Health & Medicine, Economics, Environmental Science, Philosophy, Video Games, Art & Design, Psychology, Astronomy, Politics, Mythology. Ensure each question has increasing difficulty from 1 to 8 within its set(The second question is harder than the first question, the third question is harder than the second question, etc.). Include five options (A, B, C, D, E) for each question and indicate the correct answer. Format each set as a separate JSON object.
+                {"role": "user", "content": """Generate 4 sets of 8 multiple-choice questions each. For each set, randomly select 8 topics from the following list and create one question per topic: History, Technology, General Knowledge, Science, Mathematics, Literature, Cinema & TV, Music, Geography, Sports, Computer Science, Health & Medicine, Economics, Environmental Science, Philosophy, Video Games, Art & Design, Psychology, Astronomy, Politics, Mythology, Food & Cuisine, World Cultures, Current Events, Automotive, Heavy Industry & Machine industry,  Popular Science, Future, Mind Games, Great Leaders, Language & Linguistics. Ensure each question has increasing difficulty from 1 to 8 within its set (The second question is harder than the first question, the third question is harder than the second question, etc.). Include five options (A, B, C, D, E) for each question and indicate the correct answer. For each topic, incorporate recent developments, unique historical events, or specific aspects that are less commonly known to enhance the uniqueness of the questions. 
 Expected format:
 {
   "dataset1": [
@@ -54,11 +54,11 @@ Expected format:
       "correct_answer": "C",
       "difficulty": 1
     },
-      // 7 more questions following this format
+    // 7 more questions following this format
   ],
   // Similar structure for dataset2, dataset3, and dataset4
 }
-Alert: Make sure the questions you generate are not the same questions you generated before!
+Alert: Make sure the questions you generate are not the same questions you generated before! Additionally, aim to incorporate innovative angles or perspectives in each question to ensure diversity and uniqueness. Avoid repeating themes or concepts previously used in your question sets.
 """
 
                  }
@@ -82,5 +82,5 @@ Alert: Make sure the questions you generate are not the same questions you gener
         continue  # Hata durumunda döngüyü sürdür
 
     # İsteğe bağlı: Belirli bir dosya numarası sonrası döngüyü durdur
-    if current_file_number > 25:  # Örneğin 10'dan büyük bir dosya numarası için döngüden çık
+    if current_file_number > 35:  # Örneğin 10'dan büyük bir dosya numarası için döngüden çık
         break
